a9e824b2766ec861bec5b10fbc3022aa102267e9
shadowWalker <hieu811@gmail.com>
2024-09-26 22:06:29 +0700

    feat: init database for authentication (#3)
    * init database for authentication
    
    * add jwt middleware for auth api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,5 @@
-import { getPrisma } from './prisma'
-import { base64url, EncryptJWT, jwtDecrypt } from 'jose'
 import { createMiddlewareDecorator, createParamDecorator, NextFunction, UnauthorizedException } from 'next-api-decorators'
-import { PipeMetadata } from 'next-api-decorators/dist/pipes/ParameterPipe'
+import { getToken } from 'next-auth/jwt'
 import { NextApiRequest, NextApiResponse } from 'next/types'
 import { envServer } from '@/config/env-server'
 
@@ -39,72 +37,22 @@ export function error(error: string, message?: string): ApiResponse<any> {
 }
 
 export const SessionUser = createParamDecorator<AuthUser>((req) => req.user)
+export const SessionUserId = createParamDecorator<string>((req) => (req as any).userId)
 
 export function ApiExceptionHandler(error: unknown, req: NextApiRequest, res: NextApiResponse) {
-  console.error(error)
   const errorStr = error instanceof Error ? error.message : 'An unknown error occurred.'
   res.status(200).json({ status: false, error: errorStr })
 }
 
 export const NextAuthGuard = createMiddlewareDecorator(
   async (req: NextApiRequest, res: NextApiResponse, next: NextFunction) => {
-    const tokenCookie = req.cookies['next-auth.session-token'] || req.cookies['__Secure-next-auth.session-token']
-    if (!tokenCookie) {
-      console.error('Missing token cookie', req.cookies)
-      throw new UnauthorizedException()
+    const token = await getToken({ req, secret: envServer.NEXTAUTH_SECRET })
+    if (!token) {
+      throw new UnauthorizedException('Unauthorized')
     }
-    const user = await getPrisma().user.findFirst({
-      where: {
-        sessions: {
-          some: {
-            sessionToken: tokenCookie,
-          },
-        },
-      },
-    })
-    if (!user) {
-      throw new UnauthorizedException()
-    }
-    // if (user.status != UserStatus.Actived) {
-    //   throw new UnauthorizedException()
-    // }
 
-    req.user = {
-      id: user.id,
-      name: user.name as string,
-      email: user.email as string,
-      image: user.image,
-    }
+    ;(req as any).userId = (token as any).user.id
+
     return next()
   }
 )
-
-const JwtSecret = base64url.decode(envServer.NEXTAUTH_SECRET || '')
-
-/** Validates and transforms `JwtToken` string */
-export function JwtTokenPipe() {
-  return async (value: any, metadata?: PipeMetadata) => {
-    if (typeof value !== 'string') {
-      throw new Error('JwtTokenPipe requires a string value')
-    }
-    // decode jwt with Jose
-    const { payload } = await jwtDecrypt(value, JwtSecret, {
-      issuer: metadata?.name || '',
-    })
-
-    return payload
-  }
-}
-
-export async function JwtTokenEncrypt(data: any, expires: number = 24 * 60 * 60): Promise<string> {
-  const jwt = await new EncryptJWT(data)
-    .setExpirationTime(expires)
-    .setProtectedHeader({ alg: 'dir', enc: 'A256GCM' })
-    .encrypt(JwtSecret)
-  return jwt
-}
-
-export async function JwtTokenDecrypt(token: string): Promise<any> {
-  const { payload } = await jwtDecrypt(token, JwtSecret)
-  return payload
-}
